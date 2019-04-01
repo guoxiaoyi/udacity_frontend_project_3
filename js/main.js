889250b02befc7a49fb0a3c392cbe72f48a6dc95
GuoxiaoYi <guoxiaoyi888@gmail.com>
2019-04-01 22:17:34 +0800

    第三个作业

--- js/main.js
+++ js/main.js
@@ -8,10 +8,15 @@ var markers = []
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
-  initMap(); // added 
+  initMap(); // added
   fetchNeighborhoods();
   fetchCuisines();
 });
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('../sw.js')
+    .then(function() {console.log("Service Worker Registerd");});
+}
 
 /**
  * Fetch all neighborhoods and set their HTML.
@@ -78,7 +83,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiZ3VveGlhb3lpIiwiYSI6ImNqcWxxdzdqbTRvN3g0OHFxbHF4dHo3YnQifQ.Ve6e6pTcifXhqO9v6yfXXw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -197,7 +202,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 
-} 
+}
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
